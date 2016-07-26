9fc93cdc913c181e4f36230bfba31d0a96431d86
李剑扬 <li_jy@mama.cn>
2016-07-26 16:58:05 +0800

    加入管理后台的js

---
+++ resources/views/admin/menu.blade.php
@@ -0,0 +1,37 @@
+<aside class="main-sidebar">
+    <section class="sidebar">
+        <ul class="sidebar-menu">
+            <li class="header">管理菜单</li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>内容管理</span> <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>文章管理</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i>栏目管理</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>首页管理</span> <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>推荐位管理</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i>推荐位内容管理</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>内容管理</span> <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>文章管理</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i>栏目管理</a></li>
+                </ul>
+            </li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>用户管理</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>系统设置</a></li>
+        </ul>
+    </section>
+</aside>
+
